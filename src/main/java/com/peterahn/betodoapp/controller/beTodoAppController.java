2f45710717d9bd890f4015013d6cf9ca30682d85
PeterAhn <ahs3810@gmail.com>
2023-08-07 16:16:17 +0900

    :rocket: initial

--- src/main/java/com/peterahn/betodoapp/controller/beTodoAppController.java
+++ src/main/java/com/peterahn/betodoapp/controller/beTodoAppController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class beTodoAppController {
     @GetMapping("/")
-    public String helloWorld() {
+    public String ToDoApp() {
         return "To-do Application !";
     }
 }
